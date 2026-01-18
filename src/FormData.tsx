9a6eb2fe0850e91caad50602ce71e44e8756112a
ritulanand <ritulanand007@gmail.com>
2026-01-18 11:59:11 +0530

    deployment fix added-2

--- src/FormData.tsx
+++ src/FormData.tsx
@@ -1,62 +1,7 @@
 import React, { useState } from 'react';
-import { formFields } from './FormFields';
-// import { InputField } from './InputField';
 import MultiStepForm from './MultiStepForm';
 
 function FormData() {
-  interface FormDataType {
-    firstName: string;
-    lastName: string;
-    location: string;
-    phoneNumber: string;    
-    email: string;
-  }
-
-//   const [datalist, setDatalist] = useState<FormDataType[]>([]);
-//   const [errors, setErrors] = useState<Record<string, string>>({});
-
-//   const [data, setData] = useState<FormDataType>({
-//     firstName: '',
-//     lastName: '',
-//     location: '',
-//     phoneNumber: '',
-//     email: '',
-//   });
-
-//   const validate = () => {
-//     const newErrors: Record<string, string> = {};
-//     formFields.forEach(({ name, label }) => {
-//       if (!data[name as keyof typeof data]) newErrors[name] = `${label} is required`;
-//       if (name === 'email' && data[name as keyof typeof data] && !/\S+@\S+\.\S+/.test(data[name as keyof typeof data] as string))
-//         newErrors[name] = 'Invalid email';
-//       if (name === 'phoneNumber' && data[name as keyof typeof data] && !/^\d{10}$/.test(data[name as keyof typeof data] as string))
-//         newErrors[name] = 'Phone must be 10 digits';
-//     });
-//     return newErrors;
-//   };
-
-//   const handleSubmit = (e: React.FormEvent | React.MouseEvent) => {
-//     e.preventDefault();
-//     const newErrors = validate();
-//     setErrors(newErrors);
-//     if (Object.keys(newErrors).length === 0) {
-//       setDatalist((prev) => [...prev, data]);
-//       console.log(data);
-//       setData({
-//         firstName: '',
-//         lastName: '',
-//         location: '',
-//         phoneNumber: '',
-//         email: '',
-//       });
-//       console.log(datalist);
-//     }
-//   };
-
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const { name, value } = e.target;
-//     setData((prev) => ({ ...prev, [name]: value } as FormDataType));
-//   };
 
   return (
     <>
